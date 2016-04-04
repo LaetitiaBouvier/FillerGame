a895dda9ee7f289b9a9e7f3840725d4e258ad36b
StephaneTzvetkov <stephane.kov@gmail.com>
2016-04-04 10:59:12 +0200

    Ajout d'un tableau d'introduction (2ème partie ...)

--- src/AwtControl.java
+++ src/AwtControl.java
@@ -1,6 +1,7 @@
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -22,15 +23,21 @@ public class AwtControl{
 	private Panel controlPanel;
 	private Board board;
 
-	public AwtControl(){
+	public AwtControl(String tableau){
 		
+		if(tableau.equals("INTRO")){
+			this.board = new IntroBoard(300, 300);
+		}
+		if(tableau.equals("HEXA")){
+			this.board = new HexaBoard(13, "Joueur1", "Joueur2", "", "");
+		}
 		
 		prepareGUI();
 	}
 
 	public static void main(String[] args){
 		
-		AwtControl awtControlDemo = new AwtControl();
+		AwtControl awtControlDemo = new AwtControl("INTRO");
 		awtControlDemo.showEventDemo();
 	}
 
@@ -52,7 +59,6 @@ public class AwtControl{
 		mainFrame.setVisible(true);  
 	}
 
-	// Il faudrait peut ï¿½tre fractionner d'avantage cette mï¿½thode !
 	private void showEventDemo(){
 
 		controlPanel.setBackground(Color.black);
@@ -60,65 +66,85 @@ public class AwtControl{
 		
 		mainFrame.setSize(board.getHauteur()+10,board.getLargeur()+270);
 		
-		Button redButton 		= new Button("ROUGE");		redButton.setBackground(Color.red);
-		Button orangeButton 	= new Button("ORANGE");		orangeButton.setBackground(Color.orange);
-		Button yellowButton		= new Button("JAUNE");		yellowButton.setBackground(Color.yellow);
-		Button greenButton 		= new Button("VERT");		greenButton.setBackground(Color.green);
-		Button blueButton 		= new Button("BLEU");		blueButton.setBackground(Color.blue);
-		Button magentaButton	= new Button("MAGENTA");	magentaButton.setBackground(Color.magenta);
+		setButtons();
 		
-		redButton.setActionCommand("ROUGE");				redButton.addActionListener		(new ButtonClickListener()); 
-		orangeButton.setActionCommand("ORANGE");			orangeButton.addActionListener	(new ButtonClickListener()); 
-		yellowButton.setActionCommand("JAUNE");				yellowButton.addActionListener	(new ButtonClickListener());
-		greenButton.setActionCommand("VERT");				greenButton.addActionListener	(new ButtonClickListener()); 
-		blueButton.setActionCommand("BLEU");				blueButton.addActionListener	(new ButtonClickListener()); 
-		magentaButton.setActionCommand("MAGENTA");			magentaButton.addActionListener (new ButtonClickListener()); 
+		setMenu();
+
+		mainFrame.setVisible(true); 
+	}
+	
+	private void setButtons(){
 		
-		GridBagLayout layout = new GridBagLayout();
+		if(this.board.getJoueur1() != null && this.board.getJoueur2() != null){
+			
+			Button redButton 		= new Button("ROUGE");		redButton.setBackground(Color.red);
+			Button orangeButton 	= new Button("ORANGE");		orangeButton.setBackground(Color.orange);
+			Button yellowButton		= new Button("JAUNE");		yellowButton.setBackground(Color.yellow);
+			Button greenButton 		= new Button("VERT");		greenButton.setBackground(Color.green);
+			Button blueButton 		= new Button("BLEU");		blueButton.setBackground(Color.blue);
+			Button magentaButton	= new Button("MAGENTA");	magentaButton.setBackground(Color.magenta);
+			
+			redButton.setActionCommand("ROUGE");				redButton.addActionListener		(new ButtonClickListener()); 
+			orangeButton.setActionCommand("ORANGE");			orangeButton.addActionListener	(new ButtonClickListener()); 
+			yellowButton.setActionCommand("JAUNE");				yellowButton.addActionListener	(new ButtonClickListener());
+			greenButton.setActionCommand("VERT");				greenButton.addActionListener	(new ButtonClickListener()); 
+			blueButton.setActionCommand("BLEU");				blueButton.addActionListener	(new ButtonClickListener()); 
+			magentaButton.setActionCommand("MAGENTA");			magentaButton.addActionListener (new ButtonClickListener()); 
+			
+			GridBagLayout layout = new GridBagLayout();
+			
+			controlPanel.setLayout(layout);        
+			GridBagConstraints gbc = new GridBagConstraints();
+			
+			gbc.gridx = 0;
+			gbc.gridy = 0;
+			controlPanel.add((Component) board,gbc);
+			
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.gridx = 0;
+			gbc.gridy = 1;
+			controlPanel.add(redButton,gbc); 
+			
+			gbc.gridx = 0;
+			gbc.gridy = 2;
+			controlPanel.add(orangeButton,gbc); 
+			
+			gbc.gridx = 0;
+			gbc.gridy = 3;       
+			controlPanel.add(yellowButton,gbc);  
+			
+			gbc.gridx = 0;
+			gbc.gridy = 4;      
+			controlPanel.add(greenButton,gbc);
+			
+			gbc.gridx = 0;
+			gbc.gridy = 5;      
+			controlPanel.add(blueButton,gbc);
+			
+			gbc.gridx = 0;
+			gbc.gridy = 6;      
+			controlPanel.add(magentaButton,gbc);
+		}
+		else{
+			FlowLayout layout = new FlowLayout();
+			controlPanel.setLayout(layout);
+			
+			controlPanel.add((Component) board);
+		}
+	}
+	
+	private void setMenu(){
 
-		controlPanel.setLayout(layout);        
-		GridBagConstraints gbc = new GridBagConstraints();
-
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		controlPanel.add((Component) board,gbc);
-		
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		controlPanel.add(redButton,gbc); 
-
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		controlPanel.add(orangeButton,gbc); 
-
-		gbc.gridx = 0;
-		gbc.gridy = 3;       
-		controlPanel.add(yellowButton,gbc);  
-
-		gbc.gridx = 0;
-		gbc.gridy = 4;      
-		controlPanel.add(greenButton,gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 5;      
-		controlPanel.add(blueButton,gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 6;      
-		controlPanel.add(magentaButton,gbc);
-
-		//create a menu bar
+		//Création de la barre de menus
 		final MenuBar menuBar = new MenuBar();
 
-		//create menus
+		//Création des menus
 		Menu fileMenu = new Menu("File");
-		Menu editMenu = new Menu("Jouer");
-		Menu aboutMenu = new Menu("Test");
-		
-		//create menu items
-		MenuItem newMenuItem = 
-				new MenuItem("New",new MenuShortcut(KeyEvent.VK_N));
+		Menu playMenu = new Menu("Play");
+		Menu testMenu = new Menu("Test");
+
+		//Création des items du menu "File"
+		MenuItem newMenuItem = new MenuItem("New",new MenuShortcut(KeyEvent.VK_N));
 		newMenuItem.setActionCommand("New");
 
 		MenuItem openMenuItem = new MenuItem("Open");
@@ -129,30 +155,38 @@ public class AwtControl{
 
 		MenuItem exitMenuItem = new MenuItem("Exit");
 		exitMenuItem.setActionCommand("Exit");
+		
+		//Création des items du menu "Play"
+		MenuItem hexaMenuItem = new MenuItem("Hexa");
+		hexaMenuItem.setActionCommand("HEXA");
 
-
+		//Création d'un écouteur d'item, et mise sur écoute des items créés
 		MenuItemListener menuItemListener = new MenuItemListener();
 
 		newMenuItem.addActionListener(menuItemListener);
 		openMenuItem.addActionListener(menuItemListener);
 		saveMenuItem.addActionListener(menuItemListener);
 		exitMenuItem.addActionListener(menuItemListener);
-		
-		//add menu items to menus
+		hexaMenuItem.addActionListener(menuItemListener);
+
+		//Ajout des items au menu "File"
 		fileMenu.add(newMenuItem);
 		fileMenu.add(openMenuItem);
 		fileMenu.add(saveMenuItem);
 		fileMenu.addSeparator();
 		fileMenu.add(exitMenuItem);
-
-		//add menu to menubar
-		menuBar.add(fileMenu);
-		menuBar.add(editMenu);
-		menuBar.add(aboutMenu);
-
-		//add menubar to the frame
-		mainFrame.setMenuBar(menuBar);
 		
+		//AJout des items au menu "Play"
+		playMenu.add(hexaMenuItem);
+
+		//Ajout des menus à la barre de menus
+		menuBar.add(fileMenu);
+		menuBar.add(playMenu);
+		menuBar.add(testMenu);
+
+		//Ajout de la barre de menu au cadre
+		mainFrame.setMenuBar(menuBar);
+
 		mainFrame.setVisible(true);  
 	}
 
@@ -305,9 +339,18 @@ public class AwtControl{
 	}
 	
 	class MenuItemListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {            
-			System.out.println(e.getActionCommand() 
-					+ " MenuItem clicked.");
+		public void actionPerformed(ActionEvent e) {
+			
+			System.out.println(e.getActionCommand() + " MenuItem clicked.");
+			
+			String command = e.getActionCommand();
+			
+			if(command.equals("HEXA")){
+				
+				mainFrame.dispose();
+				AwtControl awtControlDemo = new AwtControl(command);
+				awtControlDemo.showEventDemo();
+			}
 		}    
 	}
 }
