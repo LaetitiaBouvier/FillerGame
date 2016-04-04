b3466b3ec8b7f1f398642248e4a92bb62d94136e
StephaneTzvetkov <stephane.kov@gmail.com>
2016-04-04 10:58:24 +0200

    Ajout d'un tableau d'introduction

---
+++ src/IntroBoard.java
@@ -0,0 +1,54 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * L'objectif de cette classe est seulement d'offrir un joli petit menu introductif fait maison !
+ *
+ */
+public class IntroBoard extends Canvas implements Board{
+	
+	private int hauteur;
+	private int largeur;
+	
+	public IntroBoard(int hauteur, int largeur){
+		
+		this.hauteur = hauteur;
+		this.largeur = largeur;
+		
+		// Settings :
+		setBackground (Color.black);
+		setSize(hauteur, largeur);
+	}
+	
+	@Override
+	public void nextStep(Color couleur) { }
+	
+	@Override
+	public void paint(Graphics g){
+		
+		super.paint(g);
+		g.setColor(Color.white);
+		g.drawString("Bienvenue dans \" The Filler Game \"", 60, 60);
+		
+	}
+
+	@Override
+	public int getHauteur() { return this.hauteur; }
+
+	@Override
+	public int getLargeur() { return this.largeur; }
+
+	@Override
+	public Player getJoueur1() { return null; }
+
+	@Override
+	public Player getJoueur2() { return null; }
+
+	@Override
+	public Player getJoueur3() { return null; }
+
+	@Override
+	public Player getJoueur4() { return null; }
+
+}
